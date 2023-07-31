f1aa8dd6ee7281a0fb72e233661a7f52ea3356da
Nikos M <workingclasscode@gmail.com>
2023-07-31 16:12:56 +0300

    v.0.9.92
    * Image load base64 encoded image as well
    * Image handle errors uniformly

--- build/CanvasLite.js
+++ build/CanvasLite.js
@@ -2,14 +2,14 @@
 *   CanvasLite
 *   an html canvas implementation in pure JavaScript
 *
-*   @version 0.9.92 (2023-07-30 21:49:27)
+*   @version 0.9.92 (2023-07-31 16:11:27)
 *   https://github.com/foo123/CanvasLite
 *
 **//**
 *   CanvasLite
 *   an html canvas implementation in pure JavaScript
 *
-*   @version 0.9.92 (2023-07-30 21:49:27)
+*   @version 0.9.92 (2023-07-31 16:11:27)
 *   https://github.com/foo123/CanvasLite
 *
 **/
@@ -6395,13 +6395,63 @@ async function imagepng(type, img, width, height, metaData)
     return '';
 }
 
+function detect_image_type(buffer)
+{
+    // https://en.wikipedia.org/wiki/List_of_file_signatures
+    var data = new Uint8Array(buffer[buffer.subarray ? 'subarray' : 'slice'](0, 8)),
+        byteAt = function(offset) {return offset < data.length ? data[offset] : 0;};
+    if (0x89 === byteAt(0)
+        && 0x50 === byteAt(1)
+        && 0x4e === byteAt(2)
+        && 0x47 === byteAt(3)
+        && 0x0d === byteAt(4)
+        && 0x0a === byteAt(5)
+        && 0x1a === byteAt(6)
+        && 0x0a === byteAt(7)
+    ) return 'PNG';
+    else if (0x47 === byteAt(0)
+        && 0x49 === byteAt(1)
+        && 0x46 === byteAt(2)
+        && 0x38 === byteAt(3)
+        && (0x37 === byteAt(4) || 0x39 === byteAt(4))
+        && 0x61 === byteAt(5)
+    ) return 'GIF';
+    else if (0xff === byteAt(0)
+        && 0xd8 === byteAt(1)
+        /*&& 0xff === byteAt(2)
+        && 0xdb === byteAt(3)*/
+    ) return 'JPG';
+    return 'NOT_SUPPORTED';
+}
+function base64_decode(b64str)
+{
+    if ('undefined' !== typeof Buffer)
+    {
+        return Buffer.from(b64str, 'base64');
+    }
+    else if ('function' === typeof atob)
+    {
+        var binaryString = atob(b64str),
+            i, n = binaryString.length,
+            bytes = new Uint8Array(n);
+        for (i=0; i<n; ++i) bytes[i] = binaryString.charCodeAt(i);
+        return bytes.buffer;
+    }
+}
+
 function Image()
 {
-    var self = this, src = '', width = 0, height = 0, imageData = null, load;
+    var self = this, src = '', width = 0, height = 0, imageData = null, error, load;
 
+    error = function(e) {
+        if (!e instanceof Error) e = new Error(String(e));
+        if (self.onerror) self.onerror(e);
+        else throw e;
+    };
     load = function load(buffer) {
+        if (!buffer) return;
         var imgReader = Image.Reader[Image.detectImageType(buffer)];
-        if (!imgReader) err('Image file type is not supported!');
+        if (!imgReader) return error('Image file type is not supported!');
         imgReader(buffer)
         .then(function(imgData) {
             imageData = imgData;
@@ -6409,11 +6459,9 @@ function Image()
             height = imgData.height;
             if (self.onload) self.onload();
         })
-        .catch(function(error) {
-            if (self.onerror) self.onerror(error);
-            else throw error;
-        });
+        .catch(error);
     };
+
     def(self, 'width', {
         get: function() {
             return width;
@@ -6450,6 +6498,7 @@ function Image()
             if ((('undefined' !== typeof ArrayBuffer) && (file instanceof ArrayBuffer))
                 || (('undefined' !== typeof Buffer) && (file instanceof Buffer)))
             {
+                // buffer passed
                 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
                 // https://nodejs.org/api/buffer.html#class-buffer
                 load(src = file);
@@ -6457,34 +6506,30 @@ function Image()
             else if ((('undefined' !== typeof Blob) && (file instanceof Blob))
                 || (('undefined' !== typeof Buffer) && (Buffer.Blob) && (file instanceof Buffer.Blob)))
             {
+                // blob passed
                 // https://developer.mozilla.org/en-US/docs/Web/API/Blob
                 // https://nodejs.org/api/buffer.html#class-blob
-                (src = file).arrayBuffer()
-                .then(function(buffer) {
-                    load(buffer);
-                })
-                .catch(function(error) {
-                    if (self.onerror) self.onerror(error);
-                    else throw error;
-                })
+                (src = file).arrayBuffer().then(load).catch(error);
             }
-            else if (isNode && (('string' === typeof file) || (file instanceof String)))
+            else if (('string' === typeof file) || (file instanceof String))
             {
-                require('fs').readFile(src = file, function(error, buffer) {
-                    if (error)
-                    {
-                        if (self.onerror) self.onerror(error);
-                        else throw error;
-                    }
-                    else
-                    {
-                        load(buffer);
-                    }
-                });
+                if (/^data:image\/[a-z]+;base64,/.test(file))
+                {
+                    // base64 encoded image
+                    load(base64_decode((src = file).slice(file.indexOf(';base64,')+8)));
+                }
+                else if (isNode)
+                {
+                    // file path of image
+                    require('fs').readFile(src = file, function(err, buffer) {
+                        if (err) error(err);
+                        else load(buffer);
+                    });
+                }
             }
             /*else
             {
-                err('Unsupported src property');
+                error('Unsupported src property');
             }*/
         }
     });
@@ -6509,33 +6554,7 @@ Image.Reader = {
     'JPG': read_jpg,
     'PNG': read_png
 };
-Image.detectImageType = function(buffer) {
-    // https://en.wikipedia.org/wiki/List_of_file_signatures
-    var data = new Uint8Array(buffer[buffer.subarray ? 'subarray' : 'slice'](0, 8)),
-        byteAt = function(offset) {return offset < data.length ? data[offset] : 0;};
-    if (0x89 === byteAt(0)
-        && 0x50 === byteAt(1)
-        && 0x4e === byteAt(2)
-        && 0x47 === byteAt(3)
-        && 0x0d === byteAt(4)
-        && 0x0a === byteAt(5)
-        && 0x1a === byteAt(6)
-        && 0x0a === byteAt(7)
-    ) return 'PNG';
-    else if (0x47 === byteAt(0)
-        && 0x49 === byteAt(1)
-        && 0x46 === byteAt(2)
-        && 0x38 === byteAt(3)
-        && (0x37 === byteAt(4) || 0x39 === byteAt(4))
-        && 0x61 === byteAt(5)
-    ) return 'GIF';
-    else if (0xff === byteAt(0)
-        && 0xd8 === byteAt(1)
-        /*&& 0xff === byteAt(2)
-        && 0xdb === byteAt(3)*/
-    ) return 'JPG';
-    return 'NOT_SUPPORTED';
-};
+Image.detectImageType = detect_image_type;
 
 function CanvasLite(width, height)
 {
--- src/Image.js
+++ src/Image.js
@@ -1,11 +1,61 @@
 
+function detect_image_type(buffer)
+{
+    // https://en.wikipedia.org/wiki/List_of_file_signatures
+    var data = new Uint8Array(buffer[buffer.subarray ? 'subarray' : 'slice'](0, 8)),
+        byteAt = function(offset) {return offset < data.length ? data[offset] : 0;};
+    if (0x89 === byteAt(0)
+        && 0x50 === byteAt(1)
+        && 0x4e === byteAt(2)
+        && 0x47 === byteAt(3)
+        && 0x0d === byteAt(4)
+        && 0x0a === byteAt(5)
+        && 0x1a === byteAt(6)
+        && 0x0a === byteAt(7)
+    ) return 'PNG';
+    else if (0x47 === byteAt(0)
+        && 0x49 === byteAt(1)
+        && 0x46 === byteAt(2)
+        && 0x38 === byteAt(3)
+        && (0x37 === byteAt(4) || 0x39 === byteAt(4))
+        && 0x61 === byteAt(5)
+    ) return 'GIF';
+    else if (0xff === byteAt(0)
+        && 0xd8 === byteAt(1)
+        /*&& 0xff === byteAt(2)
+        && 0xdb === byteAt(3)*/
+    ) return 'JPG';
+    return 'NOT_SUPPORTED';
+}
+function base64_decode(b64str)
+{
+    if ('undefined' !== typeof Buffer)
+    {
+        return Buffer.from(b64str, 'base64');
+    }
+    else if ('function' === typeof atob)
+    {
+        var binaryString = atob(b64str),
+            i, n = binaryString.length,
+            bytes = new Uint8Array(n);
+        for (i=0; i<n; ++i) bytes[i] = binaryString.charCodeAt(i);
+        return bytes.buffer;
+    }
+}
+
 function Image()
 {
-    var self = this, src = '', width = 0, height = 0, imageData = null, load;
+    var self = this, src = '', width = 0, height = 0, imageData = null, error, load;
 
+    error = function(e) {
+        if (!e instanceof Error) e = new Error(String(e));
+        if (self.onerror) self.onerror(e);
+        else throw e;
+    };
     load = function load(buffer) {
+        if (!buffer) return;
         var imgReader = Image.Reader[Image.detectImageType(buffer)];
-        if (!imgReader) err('Image file type is not supported!');
+        if (!imgReader) return error('Image file type is not supported!');
         imgReader(buffer)
         .then(function(imgData) {
             imageData = imgData;
@@ -13,11 +63,9 @@ function Image()
             height = imgData.height;
             if (self.onload) self.onload();
         })
-        .catch(function(error) {
-            if (self.onerror) self.onerror(error);
-            else throw error;
-        });
+        .catch(error);
     };
+
     def(self, 'width', {
         get: function() {
             return width;
@@ -54,6 +102,7 @@ function Image()
             if ((('undefined' !== typeof ArrayBuffer) && (file instanceof ArrayBuffer))
                 || (('undefined' !== typeof Buffer) && (file instanceof Buffer)))
             {
+                // buffer passed
                 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
                 // https://nodejs.org/api/buffer.html#class-buffer
                 load(src = file);
@@ -61,34 +110,30 @@ function Image()
             else if ((('undefined' !== typeof Blob) && (file instanceof Blob))
                 || (('undefined' !== typeof Buffer) && (Buffer.Blob) && (file instanceof Buffer.Blob)))
             {
+                // blob passed
                 // https://developer.mozilla.org/en-US/docs/Web/API/Blob
                 // https://nodejs.org/api/buffer.html#class-blob
-                (src = file).arrayBuffer()
-                .then(function(buffer) {
-                    load(buffer);
-                })
-                .catch(function(error) {
-                    if (self.onerror) self.onerror(error);
-                    else throw error;
-                })
+                (src = file).arrayBuffer().then(load).catch(error);
             }
-            else if (isNode && (('string' === typeof file) || (file instanceof String)))
+            else if (('string' === typeof file) || (file instanceof String))
             {
-                require('fs').readFile(src = file, function(error, buffer) {
-                    if (error)
-                    {
-                        if (self.onerror) self.onerror(error);
-                        else throw error;
-                    }
-                    else
-                    {
-                        load(buffer);
-                    }
-                });
+                if (/^data:image\/[a-z]+;base64,/.test(file))
+                {
+                    // base64 encoded image
+                    load(base64_decode((src = file).slice(file.indexOf(';base64,')+8)));
+                }
+                else if (isNode)
+                {
+                    // file path of image
+                    require('fs').readFile(src = file, function(err, buffer) {
+                        if (err) error(err);
+                        else load(buffer);
+                    });
+                }
             }
             /*else
             {
-                err('Unsupported src property');
+                error('Unsupported src property');
             }*/
         }
     });
@@ -113,30 +158,4 @@ Image.Reader = {
     'JPG': read_jpg,
     'PNG': read_png
 };
-Image.detectImageType = function(buffer) {
-    // https://en.wikipedia.org/wiki/List_of_file_signatures
-    var data = new Uint8Array(buffer[buffer.subarray ? 'subarray' : 'slice'](0, 8)),
-        byteAt = function(offset) {return offset < data.length ? data[offset] : 0;};
-    if (0x89 === byteAt(0)
-        && 0x50 === byteAt(1)
-        && 0x4e === byteAt(2)
-        && 0x47 === byteAt(3)
-        && 0x0d === byteAt(4)
-        && 0x0a === byteAt(5)
-        && 0x1a === byteAt(6)
-        && 0x0a === byteAt(7)
-    ) return 'PNG';
-    else if (0x47 === byteAt(0)
-        && 0x49 === byteAt(1)
-        && 0x46 === byteAt(2)
-        && 0x38 === byteAt(3)
-        && (0x37 === byteAt(4) || 0x39 === byteAt(4))
-        && 0x61 === byteAt(5)
-    ) return 'GIF';
-    else if (0xff === byteAt(0)
-        && 0xd8 === byteAt(1)
-        /*&& 0xff === byteAt(2)
-        && 0xdb === byteAt(3)*/
-    ) return 'JPG';
-    return 'NOT_SUPPORTED';
-};
+Image.detectImageType = detect_image_type;
